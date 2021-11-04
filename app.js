ada2367fdd54b6a399868f9d4ccd08acdae5d882
Carel Williams <carel@live.se>
2021-11-04 15:49:03 +0100

    Adds routes to app and adds collection parameter to db file

--- app.js
+++ app.js
@@ -1,56 +1,13 @@
-import express from "express"
-
-const SERVER_ERROR = "Server Error"
+import express from 'express'
+import router from './routes.js'
 
 const createApp = async (db) => {
-    const app = express()
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
+  const app = express()
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(router)
 
-    app.get('/students', async (req, res) => {
-        try {
-            const students = await db.getAll()
-            res.send(students)
-        } catch (err) {
-            console.error("Error GET /students", err)
-            res.status(501).send(SERVER_ERROR)
-        }
-    })
-
-    app.get('/students/:id', async (req, res) => {
-        const id = req.params.id
-        try {
-            const student = await db.getOne(id)
-            res.send(student)
-        } catch (err) {
-            console.error("Error GET /students/id", err)
-            res.status(501).send(SERVER_ERROR)
-        }
-    })
-
-    app.post('/students', async (req, res) => {
-        const { name, age } = req.body
-        try {
-            await db.createOne({ name, age })
-            res.status(201).send({ created: true })
-        } catch (err) {
-            console.error("Error POST /students", err)
-            res.status(501).send(SERVER_ERROR)
-        }
-    })
-
-    app.delete('/students/:id', async (req, res) => {
-        const id = req.params.id
-        try {
-            await db.deleteOne(id)
-            res.status(200).send({ deleted: true })
-        } catch (err) {
-            console.error("Error POST /students", err)
-            res.status(501).send(SERVER_ERROR)
-        }
-    })
-
-    return app;
+  return app
 }
 
-export default createApp;
+export default createApp
